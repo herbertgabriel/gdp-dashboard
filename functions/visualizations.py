fecd2c19c4d7092c5273bcd1b9a220b2d3203097
Herbert <142347988+herbertgabriel@users.noreply.github.com>
2024-09-27 21:25:59 +0000

    fixed

--- functions/visualizations.py
+++ functions/visualizations.py
@@ -2,85 +2,132 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def voos_por_companhias(dados):
-    st.title("Número de Voos por Companhias Aéreas")
-    st.subheader("Este gráfico mostra a quantidade de voos por companhia aérea. "
-                  "Você pode observar quais companhias têm mais operações e como se "
-                  "comparam entre si.")
 
+def voos_por_companhias(dados, dados_tratados):
+    st.title("Número de Voos por Companhias Aéreas")
+
+    st.subheader("Dados Originais")
     plt.figure(figsize=(10, 6))
     sns.countplot(x='Airline', data=dados)
-    plt.title('Quantidade de Voos por Companhias Aéreas', fontsize=16)
+    plt.title(
+        'Quantidade de Voos por Companhias Aéreas (Dados Originais)', fontsize=16)
     plt.xlabel('Companhia Aérea', fontsize=14)
     plt.ylabel('Quantidade de Voos', fontsize=14)
-    
     st.pyplot(plt)
-    plt.close()  # Fecha a figura
+    plt.close()
 
-def voos_por_semana(dados):
+    st.subheader("Dados Tratados")
+    plt.figure(figsize=(10, 6))
+    sns.countplot(x='Airline', data=dados_tratados)
+    plt.title(
+        'Quantidade de Voos por Companhias Aéreas (Dados Tratados)', fontsize=16)
+    plt.xlabel('Companhia Aérea', fontsize=14)
+    plt.ylabel('Quantidade de Voos', fontsize=14)
+    st.pyplot(plt)
+    plt.close()
+
+
+def voos_por_semana(dados, dados_tratados):
     st.title("Número de Voos por Dia da Semana")
-    st.subheader("Este gráfico ilustra o número total de voos realizados em cada dia da semana.")
 
+    st.subheader("Dados Originais")
     plt.figure(figsize=(10, 6))
     sns.countplot(data=dados, x='DayOfWeek')
-    plt.title('Número de Voos por Dia da Semana', fontsize=16)
+    plt.title('Número de Voos por Dia da Semana (Dados Originais)', fontsize=16)
     plt.xlabel('Dia da Semana', fontsize=14)
     plt.ylabel('Número de Voos', fontsize=14)
-    
     st.pyplot(plt)
-    plt.close()  # Fecha a figura
+    plt.close()
 
-def qtd_atrasos(dados):
+    st.subheader("Dados Tratados")
+    plt.figure(figsize=(10, 6))
+    sns.countplot(data=dados_tratados, x='DayOfWeek')
+    plt.title('Número de Voos por Dia da Semana (Dados Tratados)', fontsize=16)
+    plt.xlabel('Dia da Semana', fontsize=14)
+    plt.ylabel('Número de Voos', fontsize=14)
+    st.pyplot(plt)
+    plt.close()
+
+
+def qtd_atrasos(dados, dados_tratados):
     st.title("Quantidade de Voos Atrasados")
-    st.subheader("Este gráfico mostra a quantidade de voos que se atrasaram e os que não se atrasaram.")
 
+    st.subheader("Dados Originais")
     plt.figure(figsize=(10, 6))
     sns.countplot(x='Delay', data=dados)
-    plt.title('Gráfico de Atraso de Voos', fontsize=16)
+    plt.title('Gráfico de Atraso de Voos (Dados Originais)', fontsize=16)
     plt.xlabel('(0 = Não Atrasou, 1 = Atrasou)', fontsize=14)
     plt.ylabel('Quantidade de Voos', fontsize=14)
-    
     st.pyplot(plt)
-    plt.close()  # Fecha a figura
+    plt.close()
 
     quantidade_atrasos = dados[dados['Delay'] == 1].shape[0]
-    st.write(f"Quantidade de voos que se atrasaram: **{quantidade_atrasos}**")
+    st.write(
+        f"Quantidade de voos que se atrasaram (Dados Originais): **{quantidade_atrasos}**")
 
-def atraso_por_companhia(dados):
+    st.subheader("Dados Tratados")
+    plt.figure(figsize=(10, 6))
+    sns.countplot(x='Delay', data=dados_tratados)
+    plt.title('Gráfico de Atraso de Voos (Dados Tratados)', fontsize=16)
+    plt.xlabel('(0 = Não Atrasou, 1 = Atrasou)', fontsize=14)
+    plt.ylabel('Quantidade de Voos', fontsize=14)
+    st.pyplot(plt)
+    plt.close()
+
+    quantidade_atrasos_tratados = dados_tratados[dados_tratados['Delay'] == 1].shape[0]
+    st.write(
+        f"Quantidade de voos que se atrasaram (Dados Tratados): **{quantidade_atrasos_tratados}**")
+
+
+def atraso_por_companhia(dados, dados_tratados):
     st.title("Número de Atrasos por Companhia Aérea")
-    st.subheader("Este gráfico mostra quantos atrasos ocorreram em cada companhia aérea.")
 
+    st.subheader("Dados Originais")
     atrasos = dados[dados['Delay'] == 1]
     media_atrasos = atrasos['Airline'].value_counts().reset_index()
     media_atrasos.columns = ['Airline', 'Number of Delays']
 
     plt.figure(figsize=(10, 6))
     sns.barplot(x='Airline', y='Number of Delays', data=media_atrasos)
-    plt.title('Número de Atrasos por Companhia Aérea', fontsize=16)
+    plt.title('Número de Atrasos por Companhia Aérea (Dados Originais)', fontsize=16)
     plt.xlabel('Companhia Aérea', fontsize=14)
     plt.ylabel('Número de Atrasos', fontsize=14)
     plt.xticks(rotation=45)
-    
     st.pyplot(plt)
-    plt.close()  # Fecha a figura
+    plt.close()
 
-def voos_atrasados_por_semana(dados):
+    st.subheader("Dados Tratados")
+    atrasos_tratados = dados_tratados[dados_tratados['Delay'] == 1]
+    media_atrasos_tratados = atrasos_tratados['Airline'].value_counts(
+    ).reset_index()
+    media_atrasos_tratados.columns = ['Airline', 'Number of Delays']
+
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='Airline', y='Number of Delays', data=media_atrasos_tratados)
+    plt.title('Número de Atrasos por Companhia Aérea (Dados Tratados)', fontsize=16)
+    plt.xlabel('Companhia Aérea', fontsize=14)
+    plt.ylabel('Número de Atrasos', fontsize=14)
+    plt.xticks(rotation=45)
+    st.pyplot(plt)
+    plt.close()
+
+
+def voos_atrasados_por_semana(dados, dados_tratados):
     st.title("Número de Atrasos por Dia da Semana")
-    st.subheader("Este gráfico ilustra a quantidade de atrasos por dia da semana.")
 
+    st.subheader("Dados Originais")
     atrasos = dados[dados['Delay'] == 1]
     media_atrasos = atrasos['DayOfWeek'].value_counts().reset_index()
     media_atrasos.columns = ['DayOfWeek', 'NumberDelays']
 
     plt.figure(figsize=(10, 6))
     sns.barplot(x='DayOfWeek', y='NumberDelays', data=media_atrasos)
-    plt.title('Número de Atrasos por Dia da Semana', fontsize=16)
+    plt.title('Número de Atrasos por Dia da Semana (Dados Originais)', fontsize=16)
     plt.xlabel('Dia da Semana', fontsize=14)
     plt.ylabel('Número de Atrasos', fontsize=14)
     plt.xticks(rotation=45)
-    
     st.pyplot(plt)
-    plt.close()  # Fecha a figura
+    plt.close()
 
     # Mapeia os dias da semana para nomes
     dias_da_semana = {
@@ -93,6 +140,29 @@ def voos_atrasados_por_semana(dados):
         7: "Sábado"
     }
 
-    dia_mais_atrasado = media_atrasos.loc[media_atrasos['NumberDelays'].idxmax()]
+    dia_mais_atrasado = media_atrasos.loc[media_atrasos['NumberDelays'].idxmax(
+    )]
     dia_nome = dias_da_semana[dia_mais_atrasado['DayOfWeek']]
-    st.write(f"Dia da semana com mais atrasos: **{dia_nome}** com **{dia_mais_atrasado['NumberDelays']}** atrasos.")
+    st.write(
+        f"Dia da semana com mais atrasos (Dados Originais): **{dia_nome}** com **{dia_mais_atrasado['NumberDelays']}** atrasos.")
+
+    st.subheader("Dados Tratados")
+    atrasos_tratados = dados_tratados[dados_tratados['Delay'] == 1]
+    media_atrasos_tratados = atrasos_tratados['DayOfWeek'].value_counts(
+    ).reset_index()
+    media_atrasos_tratados.columns = ['DayOfWeek', 'NumberDelays']
+
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='DayOfWeek', y='NumberDelays', data=media_atrasos_tratados)
+    plt.title('Número de Atrasos por Dia da Semana (Dados Tratados)', fontsize=16)
+    plt.xlabel('Dia da Semana', fontsize=14)
+    plt.ylabel('Número de Atrasos', fontsize=14)
+    plt.xticks(rotation=45)
+    st.pyplot(plt)
+    plt.close()
+
+    dia_mais_atrasado_tratado = media_atrasos_tratados.loc[media_atrasos_tratados['NumberDelays'].idxmax(
+    )]
+    dia_nome_tratado = dias_da_semana[dia_mais_atrasado_tratado['DayOfWeek']]
+    st.write(
+        f"Dia da semana com mais atrasos (Dados Tratados): **{dia_nome_tratado}** com **{dia_mais_atrasado_tratado['NumberDelays']}** atrasos.")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+import io  # Adicionando a importação do módulo io
+
 from functions.visualizations import (
     voos_por_companhias,
     voos_por_semana,
@@ -12,12 +16,49 @@ import pandas as pd
 st.set_page_config(layout='wide')
 
 # Carregar dados
+
+
 @st.cache_data
 def gerar_df():
     df = pd.read_csv('data/database_anp.csv')
     return df
 
+
 dados = gerar_df()
+
+# Tratamento de dados
+# Balanceamento de classes
+class_maj = dados[dados['Delay'] == 0]
+class_min = dados[dados['Delay'] == 1]
+
+class_maj_subamostrada = class_maj.sample(len(class_min), random_state=42)
+dados_balanceados = pd.concat([class_maj_subamostrada, class_min])
+dados_balanceados = dados_balanceados.sample(
+    frac=1, random_state=42).reset_index(drop=True)
+
+# Limite máximo para subamostragem
+max_samples = 10000
+
+
+def subamostrar_airlines(dados, col_name, max_samples):
+    # Subamostrar companhias com mais de 'max_samples' registros
+    major_airlines = dados[col_name].value_counts()[
+        lambda x: x > max_samples].index
+
+    # Subamostragem
+    dados_major = dados[dados[col_name].isin(major_airlines)].groupby(
+        col_name).sample(n=max_samples, random_state=42)
+
+    # Mantendo as companhias que já estão abaixo do limite
+    dados_other = dados[~dados[col_name].isin(major_airlines)]
+
+    # Concatenando os dados
+    return pd.concat([dados_major, dados_other]).reset_index(drop=True)
+
+
+# Aplicando a subamostragem para 'Airline'
+dados_balanceados = subamostrar_airlines(
+    dados_balanceados, 'Airline', max_samples)
 
 # Sidebar
 with st.sidebar:
@@ -25,21 +66,90 @@ with st.sidebar:
     st.image(logo, use_column_width=True)  # Exibe a imagem diretamente
 
     st.subheader('SELEÇÃO DE GRÁFICOS')
-    option = st.selectbox('Escolha um gráfico:', 
-                          ['Voos por Companhias', 
-                           'Voos por Semana', 
-                           'Quantidade de Atrasos', 
-                           'Atrasos por Companhia', 
-                           'Voos Atrasados por Semana'])
+    option = st.selectbox('Escolha um gráfico:', [
+        'Inicio',
+        'Voos por Companhias',
+        'Voos por Semana',
+        'Quantidade de Atrasos',
+        'Atrasos por Companhia',
+        'Voos Atrasados por Semana'
+    ])
 
-# Exibir gráfico baseado na seleção
-if option == 'Voos por Companhias':
-    voos_por_companhias(dados)
+
+def inicio():
+    st.title("Já pode voar")
+    st.subheader("Prever atrasos de voos usando um conjunto de dados de companhias aéreas é uma tarefa complexa que envolve a análise de diversos fatores que podem influenciar o tempo de chegada e partida das aeronaves.")
+    st.title("Conhecendo a base de dados")
+    st.write(dados.head())
+
+    if st.button('Número de linhas e colunas'):
+        st.write(f"Número de linhas e colunas: {dados.shape}")
+
+    if st.button('Estrutura do DataFrame'):
+        buffer = io.StringIO()
+        dados.info(buf=buffer)
+        s = buffer.getvalue()
+        st.text(s)
+
+    if st.button('Tipos de dados de cada coluna'):
+        st.write("Tipos de dados de cada coluna:")
+        st.write(dados.dtypes)
+
+    if st.button('Verificar linhas duplicadas'):
+        duplicatas = dados.duplicated()
+        if duplicatas.any():
+            st.write("Há dados redundantes (linhas duplicadas):")
+            st.write(dados[duplicatas])
+            # Limpeza de dados Redundantes
+            dados.drop_duplicates(inplace=True)
+        else:
+            st.write("Não há dados redundantes (linhas duplicadas).")
+
+    if st.button('Estatísticas das colunas'):
+        st.write("Quantidade de companhias aéreas distintas:",
+                 len(dados['Airline'].unique()))
+        st.write("Quantidade de modelos de aviões distintos:",
+                 len(dados['Flight'].unique()))
+        st.write("Quantidade de aeroportos distintos (origem):",
+                 len(dados['AirportFrom'].unique()))
+        st.write("Quantidade de aeroportos distintos (destino):",
+                 len(dados['AirportTo'].unique()))
+        st.write("Quantidade total de voos:", len(dados))
+
+    if st.button('Balanceamento de classe'):
+        for col in ['Airline', 'Flight', 'AirportFrom', 'AirportTo', 'DayOfWeek', 'Time', 'Length', 'Delay']:
+            st.write(f"Value counts for {col}:")
+            st.write(dados[col].value_counts())
+            st.write("\n")
+
+    if st.button('Resumos estatísticos'):
+        st.write("Resumos estatísticos:")
+        st.write(dados[['Time', 'Length']].describe())
+
+    if st.button('Verificar valores zero na coluna Length'):
+        zero_length = dados[dados['Length'] == 0]
+        if not zero_length.empty:
+            st.write("A coluna Length contém valores iguais a zero:")
+            st.write(zero_length)
+        else:
+            st.write("A coluna Length não contém valores iguais a zero.")
+
+    if st.button('Mostrar tratamento de dados'):
+        st.write("Balanceamento de classes:")
+        st.write(dados_balanceados['Delay'].value_counts())
+        st.write("Verificando o balanceamento após subamostragem:")
+        st.write(dados_balanceados['Airline'].value_counts())
+
+
+if option == 'Inicio':
+    inicio()
+elif option == 'Voos por Companhias':
+    voos_por_companhias(dados, dados_balanceados)
 elif option == 'Voos por Semana':
-    voos_por_semana(dados)
+    voos_por_semana(dados, dados_balanceados)
 elif option == 'Quantidade de Atrasos':
-    qtd_atrasos(dados)
+    qtd_atrasos(dados, dados_balanceados)
 elif option == 'Atrasos por Companhia':
-    atraso_por_companhia(dados)
+    atraso_por_companhia(dados, dados_balanceados)
 elif option == 'Voos Atrasados por Semana':
-    voos_atrasados_por_semana(dados)
+    voos_atrasados_por_semana(dados, dados_balanceados)
